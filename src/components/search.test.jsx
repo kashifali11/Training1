fc2e982e10bf069b692ecb3abca7ca60418cd026
Kashif Ali <kashif.ali@carbonteq.com>
2021-03-26 14:59:13 +0500

    modified

---
+++ src/components/search.test.jsx
@@ -0,0 +1,12 @@
+import Store from "../redux/store.jsx"
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import Search from './search';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Provider store={Store}><Search /></Provider>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
