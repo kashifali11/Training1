4d1d7029f4912da99b069674568b2ff0c2697e70
Kashif Ali <kashif.ali@carbonteq.com>
2021-03-26 14:14:45 +0500

    modified

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import { TextField, Button, Container, makeStyles } from "@material-ui/core";
 import { connect } from "react-redux";
-import { fetchPeople, setSearchKey, resetFetch } from "../redux/actions/action.jsx";
+import {
+  fetchPeople,
+  setSearchKey,
+  resetFetch,
+} from "../redux/actions/action.jsx";
 
 const useStyles = makeStyles({
   cont: {
@@ -9,7 +13,6 @@ const useStyles = makeStyles({
     position: "fixed",
     background: "white",
     padding: 10,
-    
   },
 });
 function Search(props) {
@@ -21,28 +24,24 @@ function Search(props) {
   const handleClick = (ev) => {
     props.setKey(searchKey);
     props.reset();
-    props.fetch(1, props.nat, searchKey);
+    props.fetch(1, props.nat);
   };
   return (
     <Container className={classes.cont}>
-      <span>
-        <TextField
-          onChange={handleChange}
-          fullWidth
-          variant="outlined"
-          placeholder="Search"
-        />
-      </span>
-      <span>
-        <Button
-          onClick={handleClick}
-          style={{ marginTop: 16 }}
-          color="primary"
-          variant="contained"
-        >
-          search
-        </Button>
-      </span>
+      <TextField
+        onChange={handleChange}
+        fullWidth
+        variant="outlined"
+        placeholder="Search"
+      />
+      <Button
+        onClick={handleClick}
+        style={{ marginTop: 16 }}
+        color="primary"
+        variant="contained"
+      >
+        search
+      </Button>
     </Container>
   );
 }
@@ -56,7 +55,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     setKey: (key) => dispatch(setSearchKey(key)),
     reset: () => dispatch(resetFetch()),
-    fetch: (page, nat, key) => dispatch(fetchPeople(page, nat, key)),
+    fetch: (page, nat) => dispatch(fetchPeople(page, nat)),
   };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Search);
